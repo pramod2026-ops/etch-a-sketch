999429233a44bbba7bac8198cca115e2c4d02309
Pramod Ghimire <275212426+pramod2026-ops@users.noreply.github.com>
2026-09-03 18:58:47 +0900

    Create 16*16 grid with javaScript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Etch-a-Sketch loaded!");
+const container = document.querySelector("#container");
+
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      const div = document.createElement("div");
+      container.appendChild(div);
+    }
+  }
+}
+createGrid(16);
